fc133de67cdcc2b9d6aafc8c1a41e85fa6778f96
pavel-smetanin <pavel.smetanin.01@mail.ru>
2023-03-07 11:16:58 +0300

    Create a new ASP.NET MVC project

---
+++ VKRproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VKRproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
